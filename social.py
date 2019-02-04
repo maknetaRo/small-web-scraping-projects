6a80fa55ebd2ebbc9ef978d82f2eb9db57c47631
Magda <makneta@gmail.com>
2019-02-04 20:35:50 +0100

    using webbrowser
    Small script using webbrowser, sys and pyperclip libraries to open favorite social sites from command line

---
+++ social.py
@@ -0,0 +1,10 @@
+#! usr/bin/python3
+import webbrowser, sys, pyperclip
+
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = '/'.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+webbrowser.open('http://www.' + address + '.com')
